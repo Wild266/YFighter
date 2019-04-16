c3c56bdc9aecd0ab0bde45f5f89101b9e43d44d6
Wild266 <32823036+Wild266@users.noreply.github.com>
2019-04-16 09:20:38 -0400

    Create Driver1.java

---
+++ Driver1.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+
+public class Driver03{
+   public static void main(String[] args){
+      JFrame frame = new JFrame("Hailstone dumbers");
+      frame.setSize(300, 300);
+      frame.setLocation(200,100);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setContentPane(new Panel03());
+      frame.setVisible(true);
+   
+   }
+
+
+}
