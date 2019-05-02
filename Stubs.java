8b789deafb35700bdc1ce65fb2233b4ac7fdb047
Wild266 <32823036+Wild266@users.noreply.github.com>
2019-05-01 21:47:08 -0400

    Added header  and created stubs file

---
+++ Stubs.java
@@ -0,0 +1,73 @@
+public class Stubs
+{
+    public static void main(String[] args)
+    {
+        
+    }
+    public void startGUI()
+    {
+    
+    }
+    public void loadGame()
+    {
+    }
+    public bool[] getInputs()
+    {
+    }
+    public void update()
+    {
+    }
+    public void draw()
+    {
+    }
+    public void endGame()
+    {
+    }
+    public class Fighter
+    {
+    }
+    public class Platform
+    {
+    }
+    public class Driver
+    {
+    }
+    public class panel01
+    {
+    }
+    public class AirDirection implements Attack
+    {
+    }
+    public class AirDown implements Attack
+    {
+    }
+    public class AirNeutral implements Attack
+    {
+    }
+    public class CrouchDirection implements Attack
+    {
+    }
+    public class CrouchUp implements Attack
+    {
+    }
+    public class CrouchNeutral implements Attack
+    {
+    }
+    public class BasicDirection implements Attack
+    {
+    }
+    public class BasicNeutral implements Attack
+    {
+    }
+    public class BasicUp implements Attack
+    {
+    }
+    public double[] getHitBox(double fighterX, double fighterY)
+    {
+    } 
+    public double[] getKnockback()
+    {
+    } 
+
+}
+
