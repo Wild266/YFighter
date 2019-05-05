9c585df4f32f1567619bdff4166e8a30f749afd4
Wild266 <32823036+Wild266@users.noreply.github.com>
2019-05-05 13:33:18 -0400

    Update Window.java

--- Window.java
+++ Window.java
@@ -7,18 +7,27 @@ public class Window extends Canvas {
 
 	
 	private static final long serialVersionUID = -8255319694373975038L;
-	public Window(int width, int height, String title, Game game) {
-		JFrame frame = new JFrame(title);
+	public Window(int width, int height/*, Game game*/) {
 		
-		frame.setPreferredSize(new Dimension(width,height));
-		frame.setMaximumSize(new Dimension(width,height));
-		frame.setMinimumSize(new Dimension(width,height));
-		
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JFrame frame = new JFrame("Yeet Fighter");
+	        frame.setSize(600, 300);
+	        frame.setLocation(200,100);
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        frame.setContentPane(new Panel1());
+	        frame.setVisible(true);
 		frame.setResizable(false);
-		frame.setLocationRelativeTo(null);
-		frame.add(game);
-		frame.setVisible(true);
-		game.start();
+		
+		
+		
+// 		frame.setPreferredSize(new Dimension(width,height));
+// 		frame.setMaximumSize(new Dimension(width,height));
+// 		frame.setMinimumSize(new Dimension(width,height));
+		
+// 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+// 		frame.setResizable(false);
+// 		frame.setLocationRelativeTo(null);
+// 		frame.add(game);
+// 		frame.setVisible(true);
+// 		game.start();
 	}
 }
