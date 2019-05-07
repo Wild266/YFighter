cd8bd4eed0ea720ad038fcb3da390e590f05ffb3
Peengarkel <49701538+Peengarkel@users.noreply.github.com>
2019-05-06 20:32:23 -0400

    Update Driver1.java

--- Driver1.java
+++ Driver1.java
@@ -4,20 +4,21 @@
 public class Driver1{
    public static void main(String[] args){
 	   new Window();
-//               Display disp = new Display();
-//         disp.startGUI();
-//         Fighter f1;
-//         Fighter f2;
-//         Platform plat = new Platform(20);
-//         Fighter fighters[] = new Fighter[2];
-//         fighters = disp.loadGame();
-//         while (fighters[0].getHealth() > 0 && fighters[1].getHealth() > 0)
-//         {
-//             boolean ins[] = disp.getInputs();
-//             fighters = disp.update(fighters,plat,ins);
-//             disp.draw(fighters);
-//         }
-//         disp.endGame(fighters);
+	   Display disp = new Display();
+           disp.startGUI();
+           Fighter f1;
+           Fighter f2;
+           Platform plat = new Platform(20);
+           Fighter fighters[] = new Fighter[2];
+	   
+           fighters = disp.loadGame();
+           while (fighters[0].getHealth() > 0 && fighters[1].getHealth() > 0)
+           {
+               boolean ins[] = disp.getInputs();
+               fighters = disp.update(fighters,plat,ins);
+               disp.draw(fighters);
+           }
+           disp.endGame(fighters);
    }
 
 
